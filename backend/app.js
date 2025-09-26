0a62ccb9c357c1c03215f2dd2210689771c31512
prithiv26 <itz.prithiv2006@gmail.com>
2025-09-26 19:48:29 +0530

    Changed directory structure

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,6 @@ const app = express()
 app.use(express.json())
 app.use('/api/auth', authRoutes)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on PORT ${process.env.PORT}`);
 })
